83bf525891934609e9a6f1e02cd3ab83b67fa1cc
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-12-01 09:59:11 -0600

    drafted NewProduct component - form

---
+++ client/src/pages/NewProduct.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductForm from '../components/ProductForm/index';
+
+const NewProduct = () => {
+  return (
+    <div className="container">
+        <h1>This is the new product page that will hold the new product form!</h1>
+        <ProductForm/>
+    </div>
+  );
+};
+
+export default NewProduct;
