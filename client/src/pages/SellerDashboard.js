08c6948a1368598158857b54358470373a3b5391
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-12-01 09:58:53 -0600

    drafted seller dashboard page

--- client/src/pages/SellerDashboard.js
+++ client/src/pages/SellerDashboard.js
@@ -1,14 +1,62 @@
 import React from "react";
-import ProductForm from '../components/ProductForm/index';
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Button, Grid, Paper } from "@material-ui/core";
+
+import SellerItem from '../components/SellerItem/index';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+    
+  }));
+
 
 const SellerDashboard = () => {
   return (
     <div className="container">
-        <h1>This is the SELLER dashboard!</h1>
+      <Container
+        maxWidth="md"
+        style={{ backgroundColor: "#cfe8fc", height: "25vh" }}
+      >
+        <h1 style={{ textAlign: "center" }}>Welcome FIRSTNAME HERE!</h1>
+      </Container>
+      <Grid container className={useStyles.root} spacing={2}  >
+      <Grid item xs={12} style={{display: 'flex', justifyContent: 'center'}}>
+        <Button variant="contained">
+          {" "}
+          <Link to="/orderHistory"> My Purchases</Link>
+        </Button>
+      </Grid>
+        <Grid container className={useStyles.root} spacing={2} style={{display: 'flex', justifyContent: 'center'}}>
+        <Grid item xs={6} style={{display: 'flex', justifyContent: 'center'}}>
+        <SellerItem/>
+      </Grid>
+        </Grid>
+      <Grid item xs={12} style={{display: 'flex', justifyContent: 'center'}}>
+        <Button variant="contained">
+          {" "}
+          <Link to="/addProduct"> Add Product</Link>
+        </Button>
+      </Grid>
+      <Grid item xs={12} style={{display: 'flex', justifyContent: 'center'}}>
+        <Button variant="contained">
+          {" "}
+          <Link to="/seller"> View Orders</Link>
+        </Button>
+      </Grid>
+      </Grid>
+      <div style={{ margin: "1rem" }}>
         
-        <ProductForm/>
-        <img src='images/0166b6a0c1730caee84901f0c4f02337'></img>
-        <img src='images/35afa876b7c427a1dbb26d44f25fd57b'></img>
+      </div>
     </div>
   );
 };
