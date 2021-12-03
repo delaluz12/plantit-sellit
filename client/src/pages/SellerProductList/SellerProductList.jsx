07a1c86b53024ccc6ce84fa1db0f93054990c2b6
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-12-03 13:14:05 -0600

    got db data to render dynamically with useState/useEffect

--- client/src/pages/SellerProductList/SellerProductList.jsx
+++ client/src/pages/SellerProductList/SellerProductList.jsx
@@ -1,88 +1,160 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect } from "react";
 //UI imports
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
-import { productRows } from '../../components/SellerChart/dummyData';
+import { productRows } from "../../components/SellerChart/dummyData";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-import "./sellerProductList.css"
+import "./sellerProductList.css";
 
 //setUp dynamic rendering of productdata
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_SELLERPRODUCTS } from "../../utils/actions";
+import { idbPromise } from '../../utils/helpers';
 
 export default function SellerProductList() {
+  // const [state, dispatch] = useStoreContext();
+
+
+  // const [rowData, setRowData] = useState([]);
+
+  // const {sellerProducts} = state;
+  // console.log(state);
+  // const { loading, data } = useQuery(QUERY_PRODUCTS);
+  // console.log(data);
+  // const {products} = data;
+  // console.log (products);
+  // const rawData = data;
+  // {
+  //   id: "",
+  //   name: "",
+  //   img: "",
+  //   stock: "",
+  //   price: "",
+  // },
+  // console.log(rawData.products);
+  // const prodArr = rawData.products;
 
   
-  const {loading, data} = useQuery(QUERY_PRODUCTS);
-  console.log(data)
+  // console.log(rowData);
+
+  
+  // const [state, dispatch] = useStoreContext();
+  // const { sellerProducts, clean } = state;
+  // console.log(sellerProducts);
+  
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+//   const updatedSellerProducts = sellerProducts.map(p => {
+//     return {
+//       id: p._id,
+//       price: p.price,
+//       image: p.image,
+//       name: p.name,
+//       quantity: p.quantity
+//     }
+//   })
+// console.log(updatedSellerProducts)
+
+const [dummyData, setDummyData] = useState([]);
+// console.log(dummyData)
+
+  useEffect(() => {
+    if (data) {
+      
+      const updatedSellerProducts = data.products.map(p => {
+        return {
+          id: p._id,
+          price: p.price,
+          image: p.image,
+          name: p.name,
+          quantity: p.quantity
+        }
+      });
+      // console.log(updatedSellerProducts)
+      setDummyData(updatedSellerProducts)
+      // dispatch({
+      //   type: UPDATE_SELLERPRODUCTS,
+      //   sellerProducts: updatedSellerProducts,
+      // });
+      updatedSellerProducts.forEach((product) => {
+        idbPromise('sellerProducts', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('sellerProducts', 'get').then((products) => {
+        setDummyData(products);
+      });
+    }
+    // return (updatedSellerProducts)
+  }, [data, loading]);
+  // console.log(dummyData)
+  
+
  
 
+  const handleDelete = (id) => {
+    setDummyData(dummyData.filter((item) => item.id !== id));
+  };
 
-    const [dummyData, setDummyData] = useState(productRows);
+  const columns = [
+    { field: "id", headerName: "ID", width: 200 },
+    {
+      field: "product",
+      headerName: "Product",
+      width: 400,
+      renderCell: (params) => {
+        return (
+          <div className="productListItem">
+            <img className="productListImg" src={`/images/${params.row.image}`} alt="" />
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    { field: "quantity", headerName: "qty", width: 150 },
+    // {
+    //   field: "status",
+    //   headerName: "Status",
+    //   width: 150,
+    // },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"product/" + params.row.id}>
+              <button className="productListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="productListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
 
-    const handleDelete = (id) => {
-      setDummyData(dummyData.filter((item) => item.id !== id));
-    };
-  
-    const columns = [
-      { field: "id", headerName: "ID", width: 150 },
-      {
-        field: "product",
-        headerName: "Product",
-        width: 200,
-        renderCell: (params) => {
-          return (
-            <div className="productListItem">
-              <img className="productListImg" src={params.row.img} alt="" />
-              {params.row.name}
-            </div>
-          );
-        },
-      },
-      { field: "stock", headerName: "Stock", width: 150 },
-      {
-        field: "status",
-        headerName: "Status",
-        width: 150,
-      },
-      {
-        field: "price",
-        headerName: "Price",
-        width: 160,
-      },
-      {
-        field: "action",
-        headerName: "Action",
-        width: 150,
-        renderCell: (params) => {
-          return (
-            <>
-              <Link to={"product/" + params.row.id}>
-                <button className="productListEdit">Edit</button>
-              </Link>
-              <DeleteOutline
-                className="productListDelete"
-                onClick={() => handleDelete(params.row.id)}
-              />
-            </>
-          );
-        },
-      },
-    ];
-
-    return (
-        <div className='sellerProductList'>
-             <DataGrid
+  return (
+    <div className="sellerProductList">
+      <DataGrid
         rows={dummyData}
         disableSelectionOnClick
         columns={columns}
         pageSize={10}
         checkboxSelection
       />
-        </div>
-    )
+    </div>
+  );
 }
