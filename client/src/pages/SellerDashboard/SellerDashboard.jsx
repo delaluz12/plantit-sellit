d10515e3818e7e80a8d9397294d719935fbdad9f
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-12-03 13:14:55 -0600

    added auth to seller Dash

--- client/src/pages/SellerDashboard/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard/SellerDashboard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Route} from 'react-router-dom';
+import { Route } from "react-router-dom";
 import "./sellerdash.css";
-
 
 import SellerTopbar from "../../components/SellerTopbar";
 import SellerSidebar from "../../components/SellerSidebar";
@@ -10,25 +9,45 @@ import SellerProductList from "../SellerProductList/SellerProductList";
 import NewProduct from "../NewProduct/NewProduct";
 import SellerProductItem from "../SellerProductItem/SellerProductItem";
 
+import auth from "../../utils/auth";
+// console.log(auth.getProfile())
+// const {data} = auth.getProfile()
+// console.log(data._id)
+// const userId = data._id;
+
 
 const SellerDashboard = () => {
   return (
     <>
-      <SellerTopbar />
-      <div className='mycontainer'>
-      <SellerSidebar />
-      <Route exact path="/seller/home">
-            <SellerHome />
-          </Route>
-      <Route exaxt path="/seller/products" component={SellerProductList} />  
-      <Route exact path="/seller/product/:id" component={SellerProductItem}/> 
-      <Route exact path="/seller/addProduct" component={NewProduct}/> 
-      
-
-      {/* <SellerHome/> */}
-      
-      </div>
-      
+      {auth.loggedIn() ? (
+        <>
+          <SellerTopbar />
+          <div className="mycontainer">
+            <SellerSidebar />
+            <Route exact path="/seller/home">
+              <SellerHome />
+            </Route>
+            <Route
+              exaxt
+              path="/seller/products"
+              component={SellerProductList}
+            />
+            <Route
+              exact
+              path="/seller/product/:id"
+              component={SellerProductItem}
+            />
+            <Route exact path="/seller/addProduct" component={NewProduct} />
+          </div>
+        </>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
     </>
   );
 };
