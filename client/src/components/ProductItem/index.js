f7058c07266d0b71dd81d45ae252ba1affe6c57d
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-12-07 15:41:59 -0600

    added all products link to categories for refresh of page, added responsiveness fixes to cart-was messed up from new link, removed a class in css that was messing with the cards

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -52,9 +52,9 @@ function ProductItem(item) {
     root: {
       maxWidth: 400,
     },
-    media: {
-      height: 200,
-    },
+    // media: {
+    //   height: 200,
+    // },
     
   });
   
@@ -67,8 +67,8 @@ function ProductItem(item) {
           <Link to={`/products/${_id}`}>
             <CardMedia className={classes.media}>
               <LazyLoadImage className="productImage" alt={name}
-                src={`/s3images/${image}`}
-                // src="/images/oregano.jpg"
+                // src={`/s3images/${image}`}
+                src="/images/oregano.jpg"
                 >
               
                 
