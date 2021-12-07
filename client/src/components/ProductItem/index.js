06f636beb4dfc12ccd5e054a0a3c3a88e3ff906b
RoachCoach <panjenambao@gmail.com>
2021-12-06 20:06:56 -0600

    Yey

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -67,8 +67,9 @@ function ProductItem(item) {
       <Link to={`/products/${_id}`}>
         <CardMedia className={classes.media}>
         <LazyLoadImage alt={name}
-          // src={`/s3images/${image}`}
-          src="./images/oregano.jpg">
+          src={`/s3images/${image}`}
+          //  
+          >
         
           
        
